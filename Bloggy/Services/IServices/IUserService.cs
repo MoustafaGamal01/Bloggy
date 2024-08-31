b18ee85002afb2bdaa7c2d6cab130a1b18525b72
Moustafa Gamal <Moustafa.Gamal672@gmail.com>
2024-08-31 13:42:59 +0300

    feat: Add User/Manage

---
+++ Bloggy/Services/IServices/IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace Bloggy.Services.IServices
+{
+    public interface IUserService
+    {
+        Task<ApplicationUser?> GetUserByEmail(string email);
+        Task<IList<string>> GetUserRoles(ApplicationUser applicationUser);
+        Task<IList<ApplicationUser>> GetAllUsers();
+
+    }
+}
---
+++ Bloggy/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿namespace Bloggy.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(IUnitOfWork unitOfWork,
+            UserManager<ApplicationUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public async Task<ApplicationUser?> GetUserByEmail(string email)
+        {
+            
+
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<IList<string>> GetUserRoles(ApplicationUser applicationUser)
+        {
+            return await _userManager.GetRolesAsync(applicationUser);
+        }
+
+        public async Task<IList<ApplicationUser>> GetAllUsers()
+        {
+            // get all users in UserManager
+            return await _userManager.Users.ToListAsync();
+        }
+    }
+}
